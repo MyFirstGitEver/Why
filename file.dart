37940981c6619261e6f7954d697dcf30bf82c590
MyFirstGitEver <113078180+MyFirstGitEver@users.noreply.github.com>
2023-01-18 19:19:21 +0700

    Update file.dart

--- file.dart
+++ file.dart
@@ -135,3 +135,95 @@ int calcualteExclude(List<Obj> list, String doc)
 
   return exclude;
 }
+
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:math';
+
+class Fly
+{
+  int speed, flyTime, restTime, currentPoint = 0;
+  
+  Fly(this.speed, this.flyTime, this.restTime);
+}
+
+void main()
+{
+  var flies = <Fly>[];
+
+  while(true)
+  {
+    String input = stdin.readLineSync().toString();
+
+    if(input == "")
+      break;
+
+    flies.add(processInput(input));
+  }
+
+  int maxPoint = -1;
+
+  for(int i=1;i<=2503;i++)
+  {
+    int maxDist = -1;
+
+    for(Fly fly in flies)
+    {
+      maxDist = max(f(i, fly), maxDist);
+    }
+
+    for(Fly fly in flies)
+    {
+      if(f(i, fly) == maxDist)
+      {
+        fly.currentPoint++;
+      }
+    }
+  }
+
+  for(Fly fly in flies)
+  {
+    maxPoint = max(maxPoint, fly.currentPoint);
+  }
+
+  print(maxPoint);
+}
+
+Fly processInput(String input)
+{
+  var arr = [];
+
+  for(int i=0;i<input.length;i++)
+  {
+    if(input.codeUnitAt(i) >= 0x30 && input.codeUnitAt(i) <= 0x39)
+    {
+      int p = i;
+      StringBuffer buffer = StringBuffer();
+      while(input.codeUnitAt(p) >= 0x30 && input.codeUnitAt(p) <= 0x39)
+      {
+        buffer.write(input[p]);
+        p++;
+      }
+
+      String str = buffer.toString();
+
+      if(str != "")
+      {
+        arr.add(int.parse(str));
+        i = p - 1;
+      }
+    }
+  }
+
+  return Fly(arr[0], arr[1], arr[2]);
+}
+
+int f(int n, Fly fly)
+{
+  int cnt = n ~/ (fly.flyTime + fly.restTime);
+  int left = n % (fly.flyTime + fly.restTime);
+
+  int tempMin = min(left, fly.flyTime);
+
+  return (cnt * fly.flyTime + tempMin) * fly.speed;
+}
