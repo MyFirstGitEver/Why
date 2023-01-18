4a5dc709ed806dcb51c794ced6d528d5364ed0e5
MyFirstGitEver <113078180+MyFirstGitEver@users.noreply.github.com>
2023-01-18 18:26:12 +0700

    Update file.dart

--- file.dart
+++ file.dart
@@ -1,58 +1,137 @@
-import 'dart:io';
+class Obj
+{
+  int start, end;
+
+  Obj(this.start, this.end);
+
+  bool wraps(Obj o)
+  {
+    return (o.start >= start && o.end <= end);
+  }
+}
 
 void main()
 {
-  // ab, cd, pq, or xy
-  const arr = ["ab", "cd", "pq", "xy"];
+  File("D:\\advent.txt").readAsString().then((value) 
+  {
+    var list = IndexesOf(value, ":\"red\"");
+    var objs = <Obj>[];
+
+    for(int i=list.length - 1;i>=0;i--)
+    {
+      int pos = list[i];
+
+      int start = 0;
+
+      int p = pos;
+      int checker = 1;
+
+      while(true)
+      {
+        if(value[p] == '}')
+          checker++;
+        else if(value[p] == '{')
+          checker--;
+
+        if(checker == 0 && value[p] == '{')
+          break;
+
+        p--;
+      }
+      
+      start = p;
+      p = pos;
+      
+      checker = 1;
+      while(true)
+      {
+        if(value[p] == '{')
+          checker++;
+        else if(value[p] == '}')
+          checker--;
+
+        if(checker == 0 && value[p] == '}')
+          break;
+        p++;
+      }
+
+      addNew(objs, Obj(start, p));
+    }
+
+    print(Count(value, 0, value.length) 
+      - calcualteExclude(objs, value));
+  });
+}
+
+int Count(String doc, int start, int end)
+{
   int ans = 0;
+  for(int i=start;i<end;i++)
+  {
+    if(doc.codeUnitAt(i) < 0x30 || doc.codeUnitAt(i) > 0x39)
+      continue;
+
+    StringBuffer buffer = StringBuffer();
+    if(i != 0 && doc[i - 1] == '-')
+      buffer.write('-');
+    
+    int p = i;
+    while(doc.codeUnitAt(p) >= 0x30 && doc.codeUnitAt(p) <= 0x39)
+    {
+      buffer.write(doc[p]);
+      p++;
+    }
+    
+    ans += int.parse(buffer.toString());
+    i = p - 1;
+  }
+
+  return ans;
+}
+
+List<int> IndexesOf(String doc, String term)
+{
+  var list = <int>[];
+
+  int cursor = -term.length;
 
   while(true)
   {
-    String str = stdin.readLineSync().toString();
+    cursor = doc.indexOf(term, cursor + term.length);
 
-    if(str == "")
+    if(cursor == -1)
       break;
-
-    int vowelCnt = 0;
-    bool duplicate = false, disallowed = false;
-  
-    for(var string in arr)
-    {
-      if(str.contains(string))
-      {
-        disallowed = true;
-        break;
-      }
-    }
-  
-    for(int i=0;i<str.length;i++)
-    {
-      switch(str[i])
-      {
-        case 'e':
-        case 'u':
-        case 'o':
-        case 'a':
-        case 'i':
-          vowelCnt++;
-          break;
-      }
-    }
-  
-    for(int i=0;i<str.length - 1;i++)
-    {
-      if(str[i] == str[i + 1])
-      {
-        duplicate = true;
-        break;
-      }
-    }
-  
-    if(vowelCnt >= 3 && duplicate && !disallowed)
-      ans++;
-    else
-      print("-> ${str}");
+    list.add(cursor);
   }
 
-  print(ans);
+  return list;
+}
+
+void addNew(List<Obj> list, Obj newObj)
+{
+  bool wrapped = false;
+
+  for(int i=list.length - 1;i>=0;i--)
+  {
+    if(list[i].wraps(newObj))
+    {
+      wrapped = true;
+      break;
+    }
+    else if(newObj.wraps(list[i]))
+      list.removeAt(i);
+  }
+
+  if(!wrapped)
+    list.add(newObj);
+}
+
+int calcualteExclude(List<Obj> list, String doc)
+{
+  int exclude = 0;
+
+  for(Obj o in list)
+    exclude += Count(doc, o.start, o.end);
+
+  return exclude;
 }
